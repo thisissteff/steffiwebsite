d50b2cab450c37e6413813e853cfe1210841ea8e
thisissteff <steffilimenqi@outlook.com>
2023-08-07 21:12:21 +0800

    scroll link

--- src/Components/Home/ScrollDown.jsx
+++ src/Components/Home/ScrollDown.jsx
@@ -11,7 +11,7 @@ const ScrollDown = () => {
               exit = {{ opacity: 0, y:'-50%'}}
               transition={transition1}
        className="home__scroll">
-        <Link to = "/about" className="home__scroll-button button--flex">
+        <Link to = "/steffiwebsite/about" className="home__scroll-button button--flex">
 
           <span className="home__scroll-name" >Find out more!</span>
           <i className="uil uil-angle-double-right home__scroll-arrow"></i>
